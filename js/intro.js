30e0e73220f4f7f26094f69a83d2c35c590efb36
James Lin <arigatojames@gmail.com>
2023-04-09 09:51:44 +0100

    first commit

---
+++ js/intro.js
@@ -0,0 +1,26 @@
+// External scripts
+
+function myFunction() {
+    document.getElementById("demo6").innerHTML = "Paragraph Changed.";
+}
+
+// JS statements
+
+function myTest() {
+let x, y, z;    // Statement 1
+x = 5;  //Statement 2
+y = 6;  //Statement 3
+z = x + y; // Statement 4
+
+document.getElementById("demo").innerHTML = "The value of z is " + z + "."; 
+}
+
+function myTest2() {
+
+    let a, b, c; // Declare 3 variables
+    a = 5; // Assign the value 5 to a
+    b = 3; // Assign the value 6 to b
+    c = a + b; // Assign the sum of a and b to c
+
+    document.getElementById("yes").innerHTML = c;
+}
